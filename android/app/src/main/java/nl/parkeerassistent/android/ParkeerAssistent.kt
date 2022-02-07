3f4d085a66460d04b5690af1747e846f590e0aea
Nils Brenkman <cfbrenkman@gmail.com>
2022-02-07 01:19:59 +0100

    fixed launcher icon, notification sound

--- android/app/src/main/java/nl/parkeerassistent/android/ParkeerAssistent.kt
+++ android/app/src/main/java/nl/parkeerassistent/android/ParkeerAssistent.kt
@@ -1,9 +1,11 @@
 package nl.parkeerassistent.android
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
+import android.content.ContentResolver
+import android.media.AudioAttributes
+import android.net.Uri
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -15,20 +17,27 @@ class ParkeerAssistent : Application() {
     }
 
     private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val id = getString(R.string.notification_channel_id)
-            val name = getString(R.string.notification_channel_name)
-            val description = getString(R.string.notification_channel_description)
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(id, name, importance).apply {
-                this.description = description
-            }
-            // Register the channel with the system
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+        val id = BuildConfig.NOTIFICATION_CHANNEL_ID
+        val name = getString(R.string.notification_channel_name)
+        val description = getString(R.string.notification_channel_description)
+        val importance = NotificationManagerCompat.IMPORTANCE_HIGH
+
+        val sound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + applicationContext.packageName + "/raw/car_horn")
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+            .build()
+
+        val channel = NotificationChannelCompat.Builder(id, importance)
+            .setName(name)
+            .setImportance(importance)
+            .setDescription(description)
+            .setSound(sound, audioAttributes)
+            .build()
+
+        // Register the channel with the system
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        notificationManager.createNotificationChannel(channel)
     }
 
 }
